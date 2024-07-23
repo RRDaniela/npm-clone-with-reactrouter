d770b5db310c58d4a4528c5f81a07a6ab001c56c
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2024-07-23 16:15:56 -0600

    feat: set up routing with React Router

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import SearchPage from "./pages/SearchPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      //Fall back to the default Homepage
+      {
+        index: true,
+        element: <HomePage></HomePage>,
+      },
+      { path: "/search", element: <SearchPage></SearchPage> },
+      {
+        //:name plays as wildcard, might go to /packages/react or packages/jest
+        path: "/packages/:name",
+        element: <DetailsPage></DetailsPage>,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
